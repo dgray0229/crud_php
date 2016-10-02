ade07d99196a7ae3893e5b91bade5d44383f1142
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2016-10-01 22:33:18 -0700

    connection fix

--- connection.php
+++ connection.php
@@ -1,14 +1,16 @@
 <?php
+
 $user = "root";
 $pass = "mysql";
+
 try {
     $db = new PDO("
-        mysql:hrost=localhost;
-        port=3000;
+        mysql:host=localhost;
+        port=3306;
         dbname=crud_php", 
         $user, $pass
     );
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo $e->getMessage();
     exit;
