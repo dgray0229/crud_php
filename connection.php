788f12572c5017841d4dd439f15269950efba1aa
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2016-12-28 01:07:56 -0800

    Connecting to JawsDB

--- connection.php
+++ connection.php
@@ -3,15 +3,19 @@
 try {
     require_once('inc/cred.php');
 
-    $HOST = getenv("HOST");
-    $PORT = getenv("PORT");
-    $USER = getenv("USER");
-    $PASS = getenv("PASSWORD");
+    $URL = getenv('JAWSDB_URL');
+    $dbparts = parse_url($URL);
+
+    $HOST = $dbparts['host'];
+    $PORT = $dbparts['port'];
+    $USER = $dbparts['user'];
+    $PASS = $dbparts['pass'];
+    $DATABASE = ltrim($dbparts['path'], '/');
 
     $db = new PDO(
         "mysql:host=$HOST;
         port=$PORT;
-        dbname=crud_php", 
+        dbname=$DATABASE",
         $USER, $PASS
     );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
